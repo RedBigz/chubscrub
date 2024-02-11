06cf40666019510b4a63c762cf65193b76e62283
red <red@redbigz.com>
2024-02-11 18:59:10 +1000

    Add argparse

--- scrub.py
+++ scrub.py
@@ -5,6 +5,7 @@ char_url = "https://api.chub.ai/api/characters/%s?full=true"
 char_frontend = "https://chub.ai/characters/%s"
 
 import json
+from sys import argv
 import cloudscraper
 from PIL import Image
 from os import makedirs
@@ -13,7 +14,9 @@ from shutil import rmtree
 
 from requests_toolbelt import MultipartEncoder
 
-rmtree("out")
+from argparse import ArgumentParser
+
+# rmtree("out")
 
 requests = cloudscraper.create_scraper()
 
@@ -29,62 +32,74 @@ def simple_search(limit = 100):
 def getcharpng(path):
     return Image.open(requests.get(avatar_url % path, stream=True).raw)
 
-searchresults = simple_search(1000)
+if __name__ == "__main__":
+    ap = ArgumentParser(prog="ChubScrub", description="Chub.ai Scrubber")
 
-for char in searchresults:
-    print(char)
+    ap.add_argument("-c", "--count", help="How many characters you wish to scrub.", type=int, default=100)
+    ap.add_argument("-i", "--import", help="Import all characters into Tavern.AI. CSRF must be disabled for this to work (add --disableCsrf to the command arguments in *tavern*)", default=False, action="store_true")
 
-    dirpath = join("out", char)
+    # if "-h" in argv or "--help" in argv:
+    #     ap.print_help()
+    #     exit()
 
-    makedirs(dirpath, exist_ok=True)
+    args = ap.parse_args()
 
-    charpath = join(dirpath, "char.json")
-    cardpath = join(dirpath, "card.png")
-    metapath = join(dirpath, "metadata.txt")
+    searchresults = simple_search(args.count)
 
-    ch = getchar(char)
-    chnode = ch["node"]
-    chdef = chnode["definition"]
+    for char in searchresults:
 
-    name = chdef["name"]
-    desc = chdef["description"]
-    
-    ex = chdef["example_dialogs"]
-    fm = chdef["first_message"]
-    ps = chdef["personality"]
+        print(char)
 
-    with open(metapath, "w+", encoding="utf-8") as meta:
-        meta.write(f"{name}\nChub.ai Page: {char_frontend % char}")
-    
-    jsonout = json.dumps({
-            "name": name,
-            "description": ps,
-            "avatar": "none",
-            "personality": ps,
-            "first_mes": fm,
-            "mes_example": ex,
-            "spec": "chara_card_v2",
-            "spec_version": "2.0",
-            "data": {
-                "creator_notes": desc
-            }
-        })
+        dirpath = join("out", char)
 
-    with open(charpath, "w+", encoding="utf-8") as charfile:
-        charfile.write(jsonout)
-    
-    img = getcharpng(char)
-    img.save(cardpath)
+        makedirs(dirpath, exist_ok=True)
 
-    with open(charpath, "rb") as charfile:
-        enc = MultipartEncoder({"avatar": ("avatar.json", charfile, "text/json"), "file_type": "json"})
-        rq = requests.post("http://127.0.0.1:8000/api/characters/import", headers={"Content-Type": enc.content_type, "Connection": "keep-alive", "Host": "localhost:8000"}, data=enc)
+        charpath = join(dirpath, "char.json")
+        cardpath = join(dirpath, "card.png")
+        metapath = join(dirpath, "metadata.txt")
 
-        print(rq.request.body)
+        ch = getchar(char)
+        chnode = ch["node"]
+        chdef = chnode["definition"]
 
-        print(rq.text)
-        fn = rq.json()["file_name"]
+        name = chdef["name"]
+        desc = chdef["description"]
         
-    with open(cardpath, "rb") as cardfile:
-        enc = MultipartEncoder({"ch_name": name, "avatar": ("avatar.png", cardfile, "image/png"), "json_data": jsonout, "avatar_url": f"{fn}.png", "description": desc})
-        requests.post("http://127.0.0.1:8000/api/characters/edit", headers={"Content-Type": enc.content_type, "Connection": "keep-alive", "Host": "localhost:8000"}, data=enc)
+        ex = chdef["example_dialogs"]
+        fm = chdef["first_message"]
+        ps = chdef["personality"]
+
+        with open(metapath, "w+", encoding="utf-8") as meta:
+            meta.write(f"{name}\nChub.ai Page: {char_frontend % char}")
+        
+        jsonout = json.dumps({
+                "name": name,
+                "description": ps,
+                "avatar": "none",
+                "personality": ps,
+                "first_mes": fm,
+                "mes_example": ex,
+                "spec": "chara_card_v2",
+                "spec_version": "2.0",
+                "data": {
+                    "creator_notes": desc
+                }
+            })
+
+        with open(charpath, "w+", encoding="utf-8") as charfile:
+            charfile.write(jsonout)
+        
+        img = getcharpng(char)
+        img.save(cardpath)
+
+        if args.__dict__["import"]:
+            with open(charpath, "rb") as charfile:
+                enc = MultipartEncoder({"avatar": ("avatar.json", charfile, "text/json"), "file_type": "json"})
+                rq = requests.post("http://127.0.0.1:8000/api/characters/import", headers={"Content-Type": enc.content_type, "Connection": "keep-alive", "Host": "localhost:8000"}, data=enc)
+                fn = rq.json()["file_name"]
+                
+            with open(cardpath, "rb") as cardfile:
+                enc = MultipartEncoder({"ch_name": name, "avatar": ("avatar.png", cardfile, "image/png"), "json_data": jsonout, "avatar_url": f"{fn}.png", "description": desc})
+                requests.post("http://127.0.0.1:8000/api/characters/edit", headers={"Content-Type": enc.content_type, "Connection": "keep-alive", "Host": "localhost:8000"}, data=enc)
+            
+            print("Successfully Uploaded to Tavern")
